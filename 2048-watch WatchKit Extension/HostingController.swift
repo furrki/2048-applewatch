945aa7cecc33c96bf76e5d7076596efbe2afc286
furrki <furkankaynary@gmail.com>
2019-11-26 11:43:16 +0200

    Initial Commit

---
+++ 2048-watch WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  2048-watch WatchKit Extension
+//
+//  Created by Furkan Kaynar on 26.11.2019.
+//  Copyright © 2019 Furkan Kaynar. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
